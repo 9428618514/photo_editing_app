bb58a071473ccc73e049bdcb01a490433929dfd9
Hiren Chandaliya <hirenpatel2744@gmail.com>
2024-06-08 14:16:42 +0530

    please check update code

--- lib/provider/app_image_provider.dart
+++ lib/provider/app_image_provider.dart
@@ -1,18 +1,33 @@
+import 'dart:developer';
 import 'dart:io';
+// import 'dart:ui';
 import 'package:flutter/foundation.dart';
+import 'package:image/image.dart' as img;
 import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AppImageProvider extends ChangeNotifier {
   Uint8List? currentImage;
   List<String> savedImagePaths = [];
+  double? _currentImageWidth;
+  double? _currentImageHeight;
+  Uint8List? get _currentImage => currentImage;
+  double? get currentImageWidth => _currentImageWidth;
+  double? get currentImageHeight => _currentImageHeight;
+
   changeImageFile(File image) {
     currentImage = image.readAsBytesSync();
     notifyListeners();
   }
 
-  changeImage(Uint8List image) {
+  changeImaged(Uint8List imageBytes) async {
+    currentImage = imageBytes;
+    await calculateImageDimensions(imageBytes);
+  }
+
+  changeImage(Uint8List image) async {
     currentImage = image;
+
     notifyListeners();
   }
 
@@ -53,5 +68,18 @@ class AppImageProvider extends ChangeNotifier {
       final File file = File(path);
       return file.readAsBytesSync();
     }).toList();
+  }
+
+  // void changeImage(Uint8List imageBytes) async {
+  //   currentImage = imageBytes;
+  //   await _calculateImageDimensions(imageBytes);
+  //   notifyListeners();
+  // }
+
+  Future<void> calculateImageDimensions(Uint8List imageBytes) async {
+    final img.Image image = img.decodeImage(imageBytes)!;
+    _currentImageWidth = image.width.toDouble();
+    _currentImageHeight = image.height.toDouble();
+    log('Image Width: $_currentImageWidth, Image Height: $_currentImageHeight'); // Add this line
   }
 }
--- lib/screens/nav pages/text_screen.dart
+++ lib/screens/nav pages/text_screen.dart
@@ -1,5 +1,204 @@
-import 'dart:typed_data';
+// import 'dart:typed_data';
 
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+// import 'package:lindi_sticker_widget/lindi_controller.dart';
+// import 'package:lindi_sticker_widget/lindi_sticker_widget.dart';
+// import 'package:photo_editing_app/provider/app_image_provider.dart';
+// import 'package:provider/provider.dart';
+// import 'package:text_editor/text_editor.dart';
+
+// class TextScreen extends StatefulWidget {
+//   const TextScreen({super.key});
+
+//   @override
+//   State<TextScreen> createState() => _TextScreenState();
+// }
+
+// class _TextScreenState extends State<TextScreen> {
+//   LindiController controller = LindiController(
+//       // borderColor: Colors.white,
+//       // iconColor: Colors.black,
+//       // showDone: true,
+//       // showClose: true,
+//       // showFlip: true,
+//       // showStack: true,
+//       // showLock: true,
+//       // showAllBorders: true,
+//       // shouldScale: true,
+//       // shouldRotate: true,
+//       // shouldMove: true,
+//       // minScale: 0.5,
+//       // maxScale: 4,
+//       );
+//   bool editor = false;
+
+//   late AppImageProvider appImageProvider;
+//   @override
+//   void initState() {
+//     appImageProvider = Provider.of<AppImageProvider>(context, listen: false);
+//     // controller.addWidget(
+//     //   const Text("hello"),
+//     // );
+//     super.initState();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Stack(
+//       children: [
+//         Scaffold(
+//           backgroundColor: Colors.black,
+//           appBar: AppBar(
+//             leading: const CloseButton(
+//               style:
+//                   ButtonStyle(iconColor: WidgetStatePropertyAll(Colors.white)),
+//             ),
+//             title: const Text(
+//               "Add Text",
+//               style: TextStyle(
+//                 color: Colors.white,
+//               ),
+//             ),
+//             actions: [
+//               IconButton(
+//                 onPressed: () async {
+//                   {
+//                     Uint8List? bytes = await controller.saveAsUint8List();
+//                     appImageProvider.changeImage(bytes!);
+//                     if (!mounted) return;
+//                     // ignore: use_build_context_synchronously
+//                     Navigator.pop(context);
+//                   }
+//                 },
+//                 icon: const Icon(
+//                   Icons.done,
+//                   color: Colors.white,
+//                   size: 29,
+//                 ),
+//               )
+//             ],
+//           ),
+//           body: Center(
+//             child: Consumer<AppImageProvider>(
+//                 builder: (BuildContext context, value, Widget? child) {
+//               if (value.currentImage != null) {
+//                 return LindiStickerWidget(
+//                   controller: controller,
+//                   child: Image.memory(value.currentImage!),
+//                 );
+//               }
+//               return const Center(
+//                 child: CircularProgressIndicator(),
+//               );
+//             }),
+//           ),
+//           bottomNavigationBar: GestureDetector(
+//             onTap: () {
+//               setState(() {
+//                 editor = true;
+//               });
+//             },
+//             child: Container(
+//               alignment: Alignment.center,
+//               width: double.infinity,
+//               height: 60,
+//               decoration: BoxDecoration(
+//                 color: Colors.black,
+//                 border: Border.all(width: 5, color: Colors.white),
+//                 borderRadius: BorderRadius.circular(
+//                   5,
+//                 ),
+//               ),
+//               child: Text(
+//                 "Add Text",
+//                 style: GoogleFonts.lato(
+//                   color: Colors.white,
+//                   fontSize: 20,
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+//         if (editor)
+//           Scaffold(
+//             backgroundColor: Colors.black87,
+//             body: SafeArea(
+//               child: Padding(
+//                 padding: const EdgeInsets.only(top: 10),
+//                 child: TextEditor(
+//                   fonts: const [
+//                     "BeautifulPeople",
+//                     "BeautyMountains",
+//                     "Billabong",
+//                     "BlackberryJam",
+//                     "BunchBlossoms",
+//                     "CinderelaRegular",
+//                     "Countryside",
+//                     "GrandHotel",
+//                     "Halimun",
+//                     "imagesemonJelly",
+//                     "OpenSans",
+//                     "Oswald",
+//                     "Quicksand",
+//                     "QuiteMagicalRegular",
+//                     "Tomatoes",
+//                     "TropicalAsianDemoRegular",
+//                     "VeganStyle",
+//                   ],
+//                   textStyle: const TextStyle(color: Colors.white),
+//                   minFontSize: 10,
+//                   maxFontSize: 70,
+
+//                   // textAlingment: textAlign,
+//                   decoration: EditorDecoration(
+//                     doneButton: Icon(
+//                       Icons.done,
+//                       color: Colors.white,
+//                       size: 30,
+//                     ),
+//                     fontFamily: const Icon(
+//                       Icons.title,
+//                       color: Colors.white,
+//                     ),
+//                     colorPalette:
+//                         const Icon(Icons.palette, color: Colors.white),
+//                     alignment: AlignmentDecoration(
+//                       left: const Text(
+//                         'left',
+//                         style: TextStyle(color: Colors.white),
+//                       ),
+//                       center: const Text(
+//                         'center',
+//                         style: TextStyle(color: Colors.white),
+//                       ),
+//                       right: const Text(
+//                         'right',
+//                         style: TextStyle(color: Colors.white),
+//                       ),
+//                     ),
+//                   ),
+//                   onEditCompleted: (style, align, text) {
+//                     setState(() {
+//                       editor = false;
+//                       if (text.isNotEmpty) {
+//                         controller.addWidget(Text(
+//                           text,
+//                           textAlign: align,
+//                           style: style,
+//                         ));
+//                       }
+//                     });
+//                   },
+//                 ),
+//               ),
+//             ),
+//           )
+//       ],
+//     );
+//   }
+// }
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lindi_sticker_widget/lindi_controller.dart';
@@ -17,29 +216,26 @@ class TextScreen extends StatefulWidget {
 
 class _TextScreenState extends State<TextScreen> {
   LindiController controller = LindiController(
-      // borderColor: Colors.white,
-      // iconColor: Colors.black,
-      // showDone: true,
-      // showClose: true,
-      // showFlip: true,
-      // showStack: true,
-      // showLock: true,
-      // showAllBorders: true,
-      // shouldScale: true,
-      // shouldRotate: true,
-      // shouldMove: true,
-      // minScale: 0.5,
-      // maxScale: 4,
-      );
+    iconColor: Colors.black,
+    showDone: true,
+    showClose: true,
+    showFlip: true,
+    showStack: true,
+    showLock: true,
+    showAllBorders: true,
+    shouldScale: true,
+    shouldRotate: true,
+    shouldMove: true,
+    minScale: 0.5,
+    maxScale: 4,
+  );
   bool editor = false;
 
   late AppImageProvider appImageProvider;
+
   @override
   void initState() {
     appImageProvider = Provider.of<AppImageProvider>(context, listen: false);
-    // controller.addWidget(
-    //   const Text("hello"),
-    // );
     super.initState();
   }
 
@@ -51,8 +247,8 @@ class _TextScreenState extends State<TextScreen> {
           backgroundColor: Colors.black,
           appBar: AppBar(
             leading: const CloseButton(
-              style:
-                  ButtonStyle(iconColor: WidgetStatePropertyAll(Colors.white)),
+              style: ButtonStyle(
+                  iconColor: MaterialStatePropertyAll(Colors.white)),
             ),
             title: const Text(
               "Add Text",
@@ -63,13 +259,10 @@ class _TextScreenState extends State<TextScreen> {
             actions: [
               IconButton(
                 onPressed: () async {
-                  {
-                    Uint8List? bytes = await controller.saveAsUint8List();
-                    appImageProvider.changeImage(bytes!);
-                    if (!mounted) return;
-                    // ignore: use_build_context_synchronously
-                    Navigator.pop(context);
-                  }
+                  Uint8List? bytes = await controller.saveAsUint8List();
+                  appImageProvider.changeImage(bytes!);
+                  if (!mounted) return;
+                  Navigator.pop(context);
                 },
                 icon: const Icon(
                   Icons.done,
@@ -81,17 +274,35 @@ class _TextScreenState extends State<TextScreen> {
           ),
           body: Center(
             child: Consumer<AppImageProvider>(
-                builder: (BuildContext context, value, Widget? child) {
-              if (value.currentImage != null) {
-                return LindiStickerWidget(
-                  controller: controller,
-                  child: Image.memory(value.currentImage!),
+              builder: (BuildContext context, value, Widget? child) {
+                if (value.currentImage != null) {
+                  return LayoutBuilder(
+                    builder: (context, constraints) {
+                      return Stack(
+                        children: [
+                          Image.memory(value.currentImage!),
+                          Positioned.fill(
+                            child: ClipRect(
+                              child: Container(
+                                width: constraints.maxWidth,
+                                height: constraints.maxHeight,
+                                child: LindiStickerWidget(
+                                  controller: controller,
+                                  child: Container(),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                }
+                return const Center(
+                  child: CircularProgressIndicator(),
                 );
-              }
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }),
+              },
+            ),
           ),
           bottomNavigationBar: GestureDetector(
             onTap: () {
@@ -106,9 +317,7 @@ class _TextScreenState extends State<TextScreen> {
               decoration: BoxDecoration(
                 color: Colors.black,
                 border: Border.all(width: 5, color: Colors.white),
-                borderRadius: BorderRadius.circular(
-                  5,
-                ),
+                borderRadius: BorderRadius.circular(5),
               ),
               child: Text(
                 "Add Text",
@@ -121,77 +330,99 @@ class _TextScreenState extends State<TextScreen> {
           ),
         ),
         if (editor)
-          Scaffold(
-            backgroundColor: Colors.black87,
-            body: SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: TextEditor(
-                  fonts: const [
-                    "BeautifulPeople",
-                    "BeautyMountains",
-                    "Billabong",
-                    "BlackberryJam",
-                    "BunchBlossoms",
-                    "CinderelaRegular",
-                    "Countryside",
-                    "GrandHotel",
-                    "Halimun",
-                    "imagesemonJelly",
-                    "OpenSans",
-                    "Oswald",
-                    "Quicksand",
-                    "QuiteMagicalRegular",
-                    "Tomatoes",
-                    "TropicalAsianDemoRegular",
-                    "VeganStyle",
-                  ],
-                  textStyle: const TextStyle(color: Colors.white),
-                  minFontSize: 10,
-                  maxFontSize: 70,
-
-                  // textAlingment: textAlign,
-                  decoration: EditorDecoration(
-                    doneButton: const Icon(
-                      Icons.done,
-                      color: Colors.white,
-                    ),
-                    fontFamily: const Icon(
-                      Icons.title,
-                      color: Colors.white,
-                    ),
-                    colorPalette:
-                        const Icon(Icons.palette, color: Colors.white),
-                    alignment: AlignmentDecoration(
-                      left: const Text(
-                        'left',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      center: const Text(
-                        'center',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      right: const Text(
-                        'right',
-                        style: TextStyle(color: Colors.white),
+          Consumer<AppImageProvider>(
+            builder: (BuildContext context, value, Widget? child) {
+              if (value.currentImage != null) {
+                return Scaffold(
+                  backgroundColor: Colors.black87,
+                  body: SafeArea(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Center(
+                        child: Container(
+                          width: value.currentImageWidth,
+                          height: value.currentImageHeight,
+                          color: Colors.transparent,
+                          child: TextEditor(
+                            fonts: const [
+                              "BeautifulPeople",
+                              "BeautyMountains",
+                              "Billabong",
+                              "BlackberryJam",
+                              "BunchBlossoms",
+                              "CinderelaRegular",
+                              "Countryside",
+                              "GrandHotel",
+                              "Halimun",
+                              "imagesemonJelly",
+                              "OpenSans",
+                              "Oswald",
+                              "Quicksand",
+                              "QuiteMagicalRegular",
+                              "Tomatoes",
+                              "TropicalAsianDemoRegular",
+                              "VeganStyle",
+                            ],
+                            textStyle: const TextStyle(color: Colors.white),
+                            minFontSize: 10,
+                            maxFontSize: 70,
+                            decoration: EditorDecoration(
+                              doneButton: Icon(
+                                Icons.done,
+                                color: Colors.white,
+                                size: 30,
+                              ),
+                              fontFamily: const Icon(
+                                Icons.title,
+                                color: Colors.white,
+                              ),
+                              colorPalette: const Icon(
+                                Icons.palette,
+                                color: Colors.white,
+                              ),
+                              alignment: AlignmentDecoration(
+                                left: const Text(
+                                  'left',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                center: const Text(
+                                  'center',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                right: const Text(
+                                  'right',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ),
+                            onEditCompleted: (style, align, text) {
+                              setState(() {
+                                editor = false;
+                                if (text.isNotEmpty) {
+                                  controller.addWidget(
+                                    Container(
+                                      width: value.currentImageWidth,
+                                      child: Text(
+                                        text,
+                                        textAlign: align,
+                                        style: style,
+                                      ),
+                                    ),
+                                  );
+                                }
+                              });
+                            },
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                  onEditCompleted: (style, align, text) {
-                    setState(() {
-                      editor = false;
-                      if (text.isNotEmpty) {
-                        controller.addWidget(Text(
-                          text,
-                          textAlign: align,
-                          style: style,
-                        ));
-                      }
-                    });
-                  },
-                ),
-              ),
-            ),
+                );
+              }
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            },
           )
       ],
     );
